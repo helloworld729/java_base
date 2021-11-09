e49bc9cca9f7ccb2910d0a06cb8cf68cc73ee1b4
renbingxian <renbingxian@meituan.com>
2021-11-09 19:36:26 +0800

    first commit

---
+++ src/p2_class/Employee.java
@@ -0,0 +1,41 @@
+package p2_class;
+
+import java.io.*;
+
+public class Employee{
+    // 成员变量
+    String name;
+    int age;
+    String designation;
+    double salary;
+
+    // p2_class.Employee 类的构造器
+    public Employee(String name){
+        this.name = name;
+    }
+
+    // 设置age的值
+    public void empAge(int empAge){
+        age =  empAge;
+    }
+
+    /* 设置designation的值*/
+    public void empDesignation(String empDesig){
+        designation = empDesig;
+    }
+
+    /* 设置salary的值*/
+    public void empSalary(double empSalary){
+        salary = empSalary;
+    }
+
+    /* 打印信息 */
+    public void printEmployee(){
+        System.out.println("名字:"+ name );
+        System.out.println("年龄:" + age );
+        System.out.println("职位:" + designation );
+        System.out.println("薪水:" + salary);
+    }
+}
+
+

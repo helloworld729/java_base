85a59e9d11f01f10a47b2bd761179feab2c77dde
Knight <1952933329@qq.com>
2021-11-10 09:07:15 +0800

    arrsy

--- src/mytest.java
+++ src/mytest.java
@@ -15,15 +15,7 @@ public class mytest {
     // 必须加访问修饰符 public 否则程序找不到入口
     // 必须 设置为 静态方法：只能直接访问静态变量，成员变量需要借助func
     public static void main(String[] argv){
-        mytest test = new mytest();
-        System.out.println("加法计算器");
-        System.out.println("enter 2 nums:");
-        Scanner s1 = new Scanner(System.in);
-        Scanner s2 = new Scanner(System.in);
-
-        test.setnum(s1.nextInt(), s2.nextInt());
-        int ans = test.add();
-
-        System.out.println(ans);
+        int [] a = {1,2,3};
+        System.out.println(a[1]);
     }
 }

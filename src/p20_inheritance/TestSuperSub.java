cd23fcdd31b343a22435e7be10da86e7df000a9b
renbingxian <renbingxian@meituan.com>
2021-11-24 16:04:23 +0800

    inheritance

---
+++ src/p20_inheritance/TestSuperSub.java
@@ -0,0 +1,62 @@
+package p20_inheritance;
+
+class SuperClass{
+    public int n;
+    SuperClass(){
+        System.out.println("父类无参构造");
+    }
+
+    SuperClass(int n){
+        System.out.println("父类有参构造，将父类成员设置为：" + n);
+        this.n = n;
+    }
+
+    public void introduction(){
+        System.out.println("this is Super class");
+    }
+}
+
+
+class SubClass extends SuperClass{
+    public int m, n;
+
+    SubClass(){
+        System.out.println("子类无参构造" + "\n");
+    }
+
+    SubClass(int n){
+        super(300);
+        System.out.println("子类有参构造，成员n被设置为："+n + "\n");
+        this.n = n;
+    }
+
+    // 如果父类没有对应的构造函数，会默认调用无参构造函数
+    SubClass(int m, int n){
+        System.out.println("子类有参构造，成员n被设置为："+n + "  成员m被设置为: "+m + "\n");
+        this.n = n;
+        this.m = m;
+    }
+
+    public void introduction(){
+        System.out.println("this is Sub class");
+    }
+
+    public void intro_test(){
+        // 访问父类
+        super.introduction();
+        // 访问子类
+        this.introduction();
+    }
+
+}
+
+
+public class TestSuperSub {
+    public static void main(String[] args){
+        SubClass sc1 = new SubClass();
+        SubClass sc2 = new SubClass(100);
+        SubClass sc3 = new SubClass(100, 200);
+
+        sc3.intro_test();
+    }
+}

09eb329fd6be90592ae8af2669724f318a8a6d24
renbingxian <renbingxian@meituan.com>
2021-12-01 11:08:57 +0800

    interface

---
+++ src/p25_Interface/MammalInt.java
@@ -0,0 +1,32 @@
+package p25_Interface;
+
+
+interface Animal{
+
+    public void eat();
+
+    public void travel();
+}
+
+
+public class MammalInt implements Animal{
+    public void eat(){
+        System.out.println("Mammal eats");
+    }
+
+    public void travel(){
+        System.out.println("Mammal travels");
+    }
+
+    public int nooflegs(){
+        return 0;
+    }
+
+    public static void main(String[] args){
+        MammalInt m = new MammalInt();
+        m.eat();
+        m.travel();
+    }
+}
+
+

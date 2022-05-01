f93e9526f5994128cfa231a720cc3f279c09da62
renbingxian <renbingxian@meituan.com>
2022-05-01 13:12:31 +0800

    update

--- src/p30_arraylist/MyArrayList.java
+++ src/p30_arraylist/MyArrayList.java
@@ -5,19 +5,45 @@ import java.util.Collections;
 
 public class MyArrayList {
     public static void main(String[] args){
-        ArrayList<String> sites = new ArrayList<String>();  // <>
+        // 动态数组初始化
+        ArrayList<String> sites = new ArrayList<String>();  // 类型必须是引用类型/封装类型
+
+        // 增加元素-add
+        System.out.println("增加元素-add");
         sites.add(0,"baidu");
-        sites.add(0, "xinliang");  // 增
+        sites.add(0, "xinlang");  // 指定索引添加
         sites.add(0, "weibo");
+        sites.add("google"); // 同 append
+
         System.out.println(sites);
-        System.out.println(sites.get(1));  // 查
-
-        sites.set(1, "hahaha");   // 改
+        // 查找元素-get：基于索引查找
+        System.out.println("查找index=1:");
         System.out.println(sites.get(1));
+        // 查找索引-indexof：基于内容查找索引，没有时返回-1
+        System.out.println("查找weibo的索引");
+        System.out.println(sites.indexOf("weibo"));
+        System.out.println("查找hello的索引");
+        System.out.println(sites.indexOf("hello"));
+        // 判断是否存在
+        System.out.println("判断hello是否存在");
+        System.out.println(sites.contains("hello"));
 
-        sites.remove(1); // 删
-        System.out.println(sites.get(1));
+        // 修改元素-set
+        sites.set(1, "hahaha");
+        System.out.println("将索引=1的位置，元素修改为hahaha");
+        System.out.println(sites.get(1)+"\n");
 
+        // 删除元素-remove：根据索引删除
+        System.out.println("删除元素-remove：根据索引删除");
+        System.out.println(sites);
+        sites.remove(1);
+        System.out.println(sites);
+        System.out.println("删除元素-removeIf：根据条件删除");
+        sites.removeIf(e -> e == "google");  // reda more about java lambda
+        System.out.println(sites+"\n");
+
+        // Iteration
+        System.out.println("Iteration + sort:");
         ArrayList<Integer> nums = new ArrayList<Integer>();  // <>
         nums.add(5);
         nums.add(2);
@@ -25,12 +51,17 @@ public class MyArrayList {
         System.out.println(nums);
         System.out.println("size: " + nums.size());
 
+        System.out.println("before sort");
         for(int j: nums){
             System.out.println(j);
         }
 
         Collections.sort(nums);
-        System.out.println(nums);
+
+        System.out.println("after sort");
+        for(int i = 0; i < nums.size(); i++){
+            System.out.println(nums.get(i));
+        }
 
     }
 }
--- src/p5_modifier/FinalTest.java
+++ src/p5_modifier/FinalTest.java
@@ -3,7 +3,7 @@ package p5_modifier;
 
 public class FinalTest {
     // 要想被静态方法main，访问，必须使用static修饰
-    private static final int get_roung(float x){
+    private static final int get_round(float x){
         return Math.round(x);
     }
 
@@ -11,6 +11,6 @@ public class FinalTest {
         final float a = 1.2f;
 //        a += 2;  // can't assign a value to final variable
 
-        System.out.println(FinalTest.get_roung(a));  // 被public修饰的类可以被当前类访问，但是在static语境下不能使用this
+        System.out.println(FinalTest.get_round(a));  // 被public修饰的类可以被当前类访问，但是在static语境下不能使用this
     }
 }

6bb9c80f86f372da99787ba07af4ae3fde8a62df
renbingxian <renbingxian@meituan.com>
2022-05-05 18:06:02 +0800

    commit

--- src/hahaha.java
+++ src/hahaha.java
@@ -1,5 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class hahaha {
     public static void main(String[] args){
+
+        String s1 = "hello world, ";
+        String s2 = "nihao";
+        System.out.println(s2.length());
+
+        System.out.println(String.format("%-20s", s1) + 222);
+        System.out.println(String.format("%-20s", s2) + 222);
+
+        System.out.println(String.format("%+5.2f", 123F));
+
 
     }
 

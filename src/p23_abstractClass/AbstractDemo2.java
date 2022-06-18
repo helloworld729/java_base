e1752ce32a08e6d25bc9d0aa5b41cf14f00dd6ce
Knight <1952933329@qq.com>
2022-06-18 16:46:34 +0800

    up

---
+++ src/p23_abstractClass/AbstractDemo2.java
@@ -0,0 +1,12 @@
+package p23_abstractClass;
+
+abstract class  Student{
+    int name;
+}
+
+public class AbstractDemo2 {
+    int[] aa = new int[5];//创建基本类型数组
+    Student[] s = new Student[5];//对象类型数
+
+
+}

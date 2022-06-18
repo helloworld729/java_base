e21a9ed03af1207bd9b4f3983b1de1b0391f4090
renbingxian <renbingxian@meituan.com>
2022-06-18 16:57:56 +0800

    u

--- src/p23_abstractClass/AbstractDemo2.java
+++ src/p23_abstractClass/AbstractDemo2.java
@@ -1,12 +1,16 @@
 package p23_abstractClass;
 
-abstract class  Student{
-    int name;
-}
+abstract class  Student{int age;}
+
+class Studenta extends Student{}
 
 public class AbstractDemo2 {
-    int[] aa = new int[5];//创建基本类型数组
-    Student[] s = new Student[5];//对象类型数
-
-
+    public static void main(String[] args) {
+        int[] a = new int[5];//创建基本类型数组
+        Student[] s = new Student[5];//对象类型数
+        a[0] = 1;
+        s[0] = new Studenta();
+        System.out.println(a[0]);
+        System.out.println(s[0]);
+    }
 }
